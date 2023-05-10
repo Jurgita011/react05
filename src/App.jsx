61705233132c8358578bb6b515cd0d49fc385fdc
Jurgita <jurgita011@gmail.com>
2023-05-10 16:48:52 +0300

    button

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.scss';
+import Buttons from './buttons';
+import './buttons.scss';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Buttons />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/buttons.jsx
@@ -0,0 +1,13 @@
+export default function Buttons() {
+
+    return (
+        <>
+        <button className="black">go</button>
+        <button className="blue">add</button>
+        <button className="red">+1</button>
+        <button className="yellow">+1</button>
+        <button>+1</button>
+        </>
+    );
+
+}
